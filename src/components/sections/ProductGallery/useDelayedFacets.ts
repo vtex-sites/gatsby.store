c8ddd48e8897c0e65d90440755f566055cca15e6
Tiago Gimenes <tlgimenes@gmail.com>
2022-04-25 10:31:14 -0300

    Initial commit

---
+++ src/components/sections/ProductGallery/useDelayedFacets.ts
@@ -0,0 +1,17 @@
+import type {
+  Filter_FacetsFragment,
+  ProductGalleryQueryQuery,
+} from '@generated/graphql'
+import { useMemo, useRef } from 'react'
+
+export const useDelayedFacets = (data?: ProductGalleryQueryQuery) => {
+  const facets = useRef<Filter_FacetsFragment[]>([])
+
+  return useMemo(() => {
+    if (data) {
+      facets.current = data.search.facets
+    }
+
+    return facets.current
+  }, [data, facets])
+}
---
+++ src/pages/account.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import { GatsbySeo } from 'gatsby-plugin-next-seo'
+
+import storeConfig from '../../store.config'
+
+function Page() {
+  useEffect(() => {
+    window.location.href = storeConfig.accountUrl
+  }, [])
+
+  return (
+    <>
+      <GatsbySeo noindex nofollow />
+
+      <div>loading...</div>
+    </>
+  )
+}
+
+export default Page
