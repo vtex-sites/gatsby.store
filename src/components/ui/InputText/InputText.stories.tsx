e86d65dffb2aa19643fb4cb24687d205dbe6fa18
renatamottam <renatamotta.m@gmail.com>
2022-04-26 15:49:13 -0300

    Adds `InputText` to Storybook

---
+++ src/components/ui/InputText/InputText.stories.tsx
@@ -0,0 +1,29 @@
+import type { InputTextProps } from '.'
+import InputText from '.'
+
+export default {
+  component: InputText,
+  title: 'Atom/InputText',
+}
+
+const Template = ({ ...args }: InputTextProps) => (
+  <div style={{ width: 400 }}>
+    <InputText {...args} />
+  </div>
+)
+
+export const Default = Template.bind({})
+export const Interactive = Template.bind({})
+
+Default.args = {
+  id: 'default-input-text',
+  label: 'Email',
+  errorMessage: 'Please, add a valid email',
+}
+
+Interactive.args = {
+  id: 'interactive-input-text',
+  label: 'Postal Code',
+  actionable: true,
+  errorMessage: 'Invalid Postal Code',
+}
