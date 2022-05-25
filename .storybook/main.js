b56639b0f63a688bf8d98b3da105a22de8ab0f6f
Lucas Feijó <lucas.portela@vtex.com.br>
2022-05-24 21:48:30 -0300

    Adds camel case support and fixes scss imports

--- .storybook/main.js
+++ .storybook/main.js
@@ -27,6 +27,28 @@ module.exports = {
       }),
     ]
 
+    // SCSS import and camelCase support for CSS modules
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              exportLocalsConvention: 'camelCase',
+            },
+          },
+        },
+        {
+          loader: 'sass-loader',
+        },
+      ],
+      include: resolve(__dirname, '../'),
+    })
+
     config.resolve.alias['@reach/router'] = resolve(
       __dirname,
       '../node_modules/@gatsbyjs/reach-router'
