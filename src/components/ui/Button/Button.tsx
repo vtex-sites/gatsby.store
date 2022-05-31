73dd71cb9c83ffeb72366a71a905327cc8ca6a01
Lucas Feijó <lucas.portela@vtex.com.br>
2022-05-31 10:11:11 -0300

    Refactors `Button` interface

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,20 +1,37 @@
 import { Button as UIButton, Icon as UIIcon } from '@faststore/ui'
-import type { ReactNode } from 'react'
-import type { ButtonProps } from '@faststore/ui'
+import type { ReactNode, AriaAttributes } from 'react'
+import type { ButtonProps as UIButtonProps } from '@faststore/ui'
 
 export type Variant = 'primary' | 'secondary' | 'tertiary'
 export type Size = 'small' | 'regular'
 export type IconPosition = 'left' | 'right'
 
-export type UIButtonProps = {
+export interface ButtonProps extends Omit<UIButtonProps, 'aria-label'> {
+  /**
+   * Specifies the component variant
+   */
   variant?: Variant
+  /**
+   * Specifies the size variant
+   */
   size?: Size
+  /**
+   * Defines use of inverted colors
+   */
   inverse?: boolean
+  /**
+   * Icon component
+   */
   icon?: ReactNode
+  /**
+   * Specifies where the icon should be positioned
+   */
   iconPosition?: IconPosition
+  /**
+   * For accessibility purposes, defines an ARIA label to the element when no label is provided
+   */
+  'aria-label'?: AriaAttributes['aria-label']
 }
-
-type Props = ButtonProps & UIButtonProps
 
 function Button({
   variant,
@@ -23,11 +40,13 @@ function Button({
   icon,
   iconPosition,
   children,
+  'aria-label': ariaLabel,
   ...props
-}: Props) {
+}: ButtonProps) {
   return (
     <UIButton
       data-fs-button
+      aria-label={ariaLabel}
       data-fs-button-variant={variant}
       data-fs-button-size={size}
       data-fs-button-inverse={inverse}
