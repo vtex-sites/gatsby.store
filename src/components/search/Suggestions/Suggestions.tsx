184c8bb2fe5e6bbb6631c1a7556f17fd66ed3756
Filipe W. Lima <filipe.lima@vtex.com.br>
2022-06-02 21:36:26 -0300

    Better organize by extracting `useSuggestions` to its own file
    Address this comment:
    https://github.com/vtex-sites/gatsby.store/pull/67#discussion_r887397864

--- src/components/search/Suggestions/Suggestions.tsx
+++ src/components/search/Suggestions/Suggestions.tsx
@@ -1,53 +1,9 @@
-import { gql } from '@vtex/graphql-utils'
 import UISuggestions from 'src/components/ui/Search/Suggestions'
-import type {
-  SearchSuggestionsQueryQuery as Query,
-  SearchSuggestionsQueryQueryVariables as Variables,
-} from '@generated/graphql'
+import useSuggestions from 'src/sdk/search/useSuggestions'
 import type { SuggestionsProps } from 'src/components/ui/Search/Suggestions'
-import { useSession } from '@faststore/sdk'
-import { useQuery } from 'src/sdk/graphql/useQuery'
 
 import { SearchHistory } from '../History'
 import SuggestionsTopSearch from './SuggestionsTopSearch'
-
-const MAX_SUGGESTIONS = 5
-
-const query = gql`
-  query SearchSuggestionsQuery(
-    $term: String!
-    $selectedFacets: [IStoreSelectedFacet!]
-  ) {
-    search(first: 5, term: $term, selectedFacets: $selectedFacets) {
-      suggestions {
-        terms {
-          value
-        }
-        products {
-          ...ProductSummary_product
-        }
-      }
-    }
-  }
-`
-
-function useSuggestions(term: string, limit: number = MAX_SUGGESTIONS) {
-  const { channel, locale } = useSession()
-
-  const { data, error } = useQuery<Query, Variables>(query, {
-    term,
-    selectedFacets: [
-      { key: 'channel', value: channel ?? '' },
-      { key: 'locale', value: locale },
-    ],
-  })
-
-  return {
-    terms: (data?.search.suggestions.terms ?? []).slice(0, limit),
-    products: (data?.search.suggestions.products ?? []).slice(0, limit),
-    isLoading: !error && !data,
-  }
-}
 
 function Suggestions({ term = '', ...otherProps }: SuggestionsProps) {
   const { terms, products, isLoading } = useSuggestions(term)
