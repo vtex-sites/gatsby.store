498ca3a80228e613eb97a3ed02161ea0f639154c
Lucas Feijó <lucas.portela@vtex.com.br>
2022-06-07 10:57:59 -0300

    Minor fixes in `ButtonSignInFallback`

---
+++ src/components/ui/Button/ButtonLink/ButtonSignIn/ButtonSignInFallback/ButtonSignInFallback.tsx
@@ -0,0 +1,18 @@
+import Icon from 'src/components/ui/Icon'
+import ButtonLink from 'src/components/ui/Button/ButtonLink'
+
+function ButtonSignInFallback() {
+  return (
+    <ButtonLink
+      data-fs-button-signin-link
+      to="/login"
+      className="text__title-mini signin-link"
+      variant="tertiary"
+    >
+      <Icon name="User" width={18} height={18} weight="bold" />
+      <span>{'Sign In'}</span>
+    </ButtonLink>
+  )
+}
+
+export default ButtonSignInFallback
