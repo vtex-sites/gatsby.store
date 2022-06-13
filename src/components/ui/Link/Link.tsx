af04ba863d86ed9cc9a5727015f65dbd1909b90a
Lucas Feijó <lucas.portela@vtex.com.br>
2022-06-13 14:56:58 -0300

    Improves external link checker by memoizing it

--- src/components/ui/Link/Link.tsx
+++ src/components/ui/Link/Link.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react'
+import { forwardRef, useMemo } from 'react'
 import type { Ref, ElementType, AnchorHTMLAttributes } from 'react'
 import { Link as GatsbyLink } from 'gatsby'
 import type { GatsbyLinkProps } from 'gatsby'
@@ -8,7 +8,7 @@ import type { LinkProps as UILinkProps } from '@faststore/ui'
 import styles from './link.module.scss'
 
 // From Gatsby's internals: https://github.com/gatsbyjs/gatsby/blob/2975c4d1271e3da52b531ad2f49261c362e5ae13/packages/gatsby-link/src/index.js#L42-L46
-function isExternalLink(href: string) {
+function externalLinkChecker(href: string) {
   return (
     href.startsWith('//') ||
     href.startsWith('http://') ||
@@ -25,31 +25,50 @@ export type LinkProps<T extends ElementType = 'a'> = UILinkProps<T> &
     href: string
     variant?: Variant
     inverse?: boolean
+    externalLink?: boolean
   }
 
 const Link = forwardRef<HTMLAnchorElement, LinkProps>(function Link<
   T extends ElementType = 'a'
 >(
-  { href, inverse, children, variant = 'default', ...otherProps }: LinkProps<T>,
+  {
+    href,
+    inverse,
+    children,
+    externalLink,
+    variant = 'default',
+    ...otherProps
+  }: LinkProps<T>,
   ref: Ref<HTMLAnchorElement> | undefined
 ) {
-  const defaultProps = {
-    'data-fs-link': true,
-    'data-fs-link-variant': variant,
-    'data-fs-link-inverse': inverse,
-    className: styles.fsLink,
-  }
+  const isExternalLink = useMemo(() => externalLinkChecker(href), [href])
 
-  if (isExternalLink(href)) {
+  if (externalLink || isExternalLink) {
     return (
-      <UILink ref={ref} href={href} {...defaultProps} {...otherProps}>
+      <UILink
+        ref={ref}
+        href={href}
+        data-fs-link
+        data-fs-link-variant={variant}
+        data-fs-link-inverse={inverse}
+        className={styles.fsLink}
+        {...otherProps}
+      >
         {children}
       </UILink>
     )
   }
 
   return (
-    <UILink as={GatsbyLink} to={href} {...defaultProps} {...otherProps}>
+    <UILink
+      as={GatsbyLink}
+      to={href}
+      data-fs-link
+      data-fs-link-variant={variant}
+      data-fs-link-inverse={inverse}
+      className={styles.fsLink}
+      {...otherProps}
+    >
       {children}
     </UILink>
   )
