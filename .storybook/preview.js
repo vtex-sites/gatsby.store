2b4db39860ab429870f069d8513ef07fb550a01c
Fanny Chien <fannychienn@gmail.com>
2022-06-17 15:54:11 -0300

    Hides actions tab

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -47,7 +47,7 @@ const customViewports = {
 }
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: { disable: true, argTypesRegex: '^on[A-Z].*' },
   controls: {
     theme: SBTheme,
   },
