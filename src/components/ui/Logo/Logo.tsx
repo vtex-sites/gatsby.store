0bcb35b1fba954430970d8fedbdc31a7d44b49f6
Tiago Gimenes <tlgimenes@gmail.com>
2022-06-20 11:45:09 -0300

    fix logo

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,6 +1,12 @@
 function Logo() {
-  return <img src="/logo.svg" alt="The store logo with with a stylized letter B in a dark blue gradient
-        background" width="176px" height="30px" />
+  return (
+    <img
+      src="/logo.svg"
+      alt="The store logo with with a stylized letter B in a dark blue gradient background"
+      width="176px"
+      height="30px"
+    />
+  )
 }
 
 export default Logo
