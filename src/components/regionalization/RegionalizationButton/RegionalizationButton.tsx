009e282f27e9a6ab6e59c46d80f0aad8620dc648
Renata Motta <renata.motta@vtex.com.br>
2022-06-27 15:02:42 -0300

    Feat: `Regionalization` Doc Storybook (#110)
    * feat: add tokens to RegionalizationBar
    
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>
    
    * feat: Add RegionalizationBar docs
    
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>
    
    * chore: Add changelog
    
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>
    
    * Fixes Storybook addon-docs warnings
    
    * Removes Stories for `RegionalizationBar`
    
    * Creates Regionalization stories
    
    * Fixes typo
    
    * Fixes typo
    
    * Improves Regionalization import text
    
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>
    
    * Fixes typo
    
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>
    
    * Fixes import text
    
    * Improves Regionalization doc page
    
    * Updates Regionalization Bar tokens title
    
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>
    
    * Creates `RegionalizationBar` doc page and simplifies its tokens
    
    * Moves `RegionalizationButton` styles to navbar
    
    * Improves Regionalization doc
    
    * Fixes Doc links
    
    * Improves copy
    
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>
    
    * Fixes regionalization tokens
    
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>
    
    * Improves `RegionalizationBar` tokens
    
    * Updates `Regionalization` Overview page with `Modal`
    
    * Updates Changelog
    
    * Fixes `Regionalization` story links
    
    * Fixes "See More" button on `Modal` story
    
    Co-authored-by: Arthur Andrade <arthurfelandrade@gmail.com>
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>

--- src/components/regionalization/RegionalizationButton/RegionalizationButton.tsx
+++ src/components/regionalization/RegionalizationButton/RegionalizationButton.tsx
@@ -2,30 +2,23 @@ import { useSession } from '@faststore/sdk'
 import Button from 'src/components/ui/Button'
 import Icon from 'src/components/ui/Icon'
 import { useUI } from 'src/sdk/ui/Provider'
-import type { HTMLAttributes } from 'react'
 
-interface RegionalizationButtonProps extends HTMLAttributes<HTMLDivElement> {
-  classes: string
-}
-
-export default function RegionalizationButton({
-  classes,
-  ...otherProps
-}: RegionalizationButtonProps) {
+function RegionButton() {
   const { openModal } = useUI()
   const { postalCode } = useSession()
 
   return (
-    <div data-fs-regionalization-button className={classes} {...otherProps}>
-      <Button
-        variant="tertiary"
-        size="small"
-        icon={<Icon name="MapPin" width={24} height={24} />}
-        iconPosition="left"
-        onClick={openModal}
-      >
-        <span>{postalCode ?? 'Set your location'}</span>
-      </Button>
-    </div>
+    <Button
+      data-fs-regionalization-button
+      variant="tertiary"
+      size="small"
+      icon={<Icon name="MapPin" width={24} height={24} />}
+      iconPosition="left"
+      onClick={openModal}
+    >
+      <span>{postalCode ?? 'Set your location'}</span>
+    </Button>
   )
 }
+
+export default RegionButton
