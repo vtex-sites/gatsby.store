96e0b70dee95876cfbd15dc1992091f314fad167
Larícia Mota <laricia.mota@vtex.com.br>
2022-07-22 15:34:42 -0300

    Remove from home page

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,7 +4,6 @@ import Footer from 'src/components/common/Footer'
 import Navbar from 'src/components/common/Navbar'
 import Toast from 'src/components/common/Toast'
 import RegionalizationBar from 'src/components/regionalization/RegionalizationBar'
-import Newsletter from 'src/components/sections/Newsletter'
 import { useUI } from 'src/sdk/ui/Provider'
 import type { PropsWithChildren } from 'react'
 
@@ -33,7 +32,6 @@ function Layout({ children }: PropsWithChildren) {
         {children}
       </main>
 
-      <Newsletter title="Newsletter" />
       <Footer />
 
       {displayCart && (
