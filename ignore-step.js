4dc1f9ef02d17b8539870b4b1e194dfbaa3ed973
Ícaro Azevedo <icazevedo10@gmail.com>
2022-07-22 15:04:15 -0300

    chore: remove ignore-step file

--- ignore-step.js
+++
@@ -1,5 +0,0 @@
-const VERSION_BUMP_COMMIT_MESSAGE_PREFIX = 'chore(release):'
-
-const commitMessage = process.env.VERCEL_GIT_COMMIT_MESSAGE || ''
-
-process.exit(commitMessage.includes(VERSION_BUMP_COMMIT_MESSAGE_PREFIX) ? 0 : 1)
