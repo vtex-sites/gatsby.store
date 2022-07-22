facc59606930b5982932481872421f9ce7a0e314
Renata Motta <renata.motta@vtex.com.br>
2022-07-22 09:47:45 -0300

    Feat: Theming Adjustments (#175)
    * Fixes `SectionListProps`
    
    * Fixes `Tiles` border-radius
    
    * Adds missing TokenColor on Interactive Controls page
    
    * Removes commented tokens from `CustomTheme`
    
    * Fixes theme class on body
    
    * Fixes `CartToggle` background color
    
    * Adds links between `SlideOver` Use Cases components
    
    * Fixes product-listing-pagination section
    
    * Removes Themes dependencies
    
    * Creates Themes pages
    
    * Fixes `Alert` bkg
    
    * Updates Changelog
    
    * Adds `TokenTable` to `Typography` page
    
    * Fixes Theme Section Storybook classes
    
    * Improves `Typography` doc page
    
    * Improves `Theme` doc pages titles
    
    Co-Authored-By: Fanny Chien <3356699+hellofanny@users.noreply.github.com>
    
    * Updates `Customizing` copy
    
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>
    
    * Updates Changelog
    
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>
    
    Co-authored-by: Fanny Chien <3356699+hellofanny@users.noreply.github.com>
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>

--- src/fonts/WebFonts.tsx
+++ src/fonts/WebFonts.tsx
@@ -1,21 +1,11 @@
-import storeConfig from 'store.config'
-
 function WebFonts() {
   return (
-    // Replace this content with the <link> of your font-family of choice
     <>
-      {storeConfig.theme === 'midnight' && (
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&display=swap"
-        />
-      )}
-      {storeConfig.theme === 'soft-blue' && (
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
-        />
-      )}
+      {/* Add a <link> tag for your font-family of choice */}
+      {/* <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
+      /> */}
     </>
   )
 }
