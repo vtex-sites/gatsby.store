98929aef4e7b5bd683b6e78de22af0aa81134764
Lucas Feijó <lucas.portela@vtex.com.br>
2022-07-29 16:30:57 -0300

    Apply patch from #171 `nextjs.store`

--- src/components/skeletons/ProductGridSkeleton/ProductGridSkeleton.tsx
+++ src/components/skeletons/ProductGridSkeleton/ProductGridSkeleton.tsx
@@ -1,17 +1,17 @@
-import { ITEMS_PER_PAGE } from 'src/constants'
 import type { PropsWithChildren } from 'react'
+import { ITEMS_PER_PAGE } from 'src/constants'
 import styles from 'src/components/product/ProductGrid/product-grid.module.scss'
 
 import ProductCardSkeleton from '../ProductCardSkeleton'
 
-interface Props {
+interface ProductGridSkeletonProps {
   loading?: boolean
 }
 
 function ProductGridSkeleton({
   children,
   loading = true,
-}: PropsWithChildren<Props>) {
+}: PropsWithChildren<ProductGridSkeletonProps>) {
   return loading ? (
     <ul className={styles.fsProductGrid}>
       {Array.from({ length: ITEMS_PER_PAGE }, (_, index) => (
