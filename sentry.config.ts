9868cc1a5f536299a78458649084912f105cb6c8
Emerson Laurentino <emerson@laurentino.co>
2022-10-27 18:04:57 -0300

    chore: using account dynamically

--- sentry.config.ts
+++ sentry.config.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/gatsby'
+import config from './store.config'
 
 Sentry.init({
   dsn: 'https://64257cdef5c74ca884d3f69099633719@o191317.ingest.sentry.io/5515975',
@@ -6,7 +7,7 @@ Sentry.init({
   debug: process.env.NODE_ENV === 'development',
   beforeSend(event) {
     event.extra = {
-      account: 'storeframework',
+      account: config.api.storeId,
       ...event.extra,
     }
     return event
