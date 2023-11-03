375320d333a9a5bf9dadfdef3747af229531cdd1
Guilherme Carvalho <guilherme.carvalho@vtex.com.br>
2023-11-03 10:04:06 -0300

    Use vtexassets.com

--- src/components/ui/Image/thumborUrlBuilder.ts
+++ src/components/ui/Image/thumborUrlBuilder.ts
@@ -19,7 +19,7 @@ export interface ThumborOptions {
   manualCrop?: Box | false
 }
 
-const THUMBOR_SERVER = 'https://assets.vtex.app'
+const THUMBOR_SERVER = 'https://vtexassets.com'
 
 const cropSection = ({ left, top, right, bottom }: Box) =>
   `${left}x${top}:${right}x${bottom}`
