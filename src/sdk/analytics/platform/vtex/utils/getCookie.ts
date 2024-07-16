63469690ff1588924111d23e71b2019813d77f13
Lucas Feijó <lucas.portela@vtex.com>
2024-07-16 18:04:44 -0300

    chore: Logs

--- src/sdk/analytics/platform/vtex/utils/getCookie.ts
+++ src/sdk/analytics/platform/vtex/utils/getCookie.ts
@@ -1,9 +1,14 @@
 export function getCookie(name: string) {
+  console.log('getCookie name', name)
   const cookieString = decodeURIComponent(document.cookie)
+  console.log('getCookie cookieString', cookieString)
   const cookies = cookieString.split(';')
+  console.log('getCookie cookies', cookies)
 
   for (const cookie of cookies) {
     const [cookieName, cookieValue] = cookie.trim().split('=')
+    console.log('getCookie cookieName', cookieName)
+    console.log('getCookie cookieValue', cookieValue)
 
     if (cookieName === name) {
       return cookieValue
